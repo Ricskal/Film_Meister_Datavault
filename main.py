4327f0445dab716769adfed3ef70f4e59a1628ab
Ricskal <rickenisnkrullen@gmail.com>
2023-04-19 14:43:32 +0200

    INIT

---
+++ main.py
@@ -0,0 +1,73 @@
+import sqlite3
+import pandas as pd
+
+
+# connect to the database
+conn = sqlite3.connect('C:\\FilmMeister\\FilmMeister.db')
+
+# ----------- CREATE & LOAD STAGING TABLE AND VIEW -----------------
+# read the Excel file into a pandas DataFrame
+df = pd.read_excel('C:\\FilmMeister\\Filmavonden.xlsx', sheet_name='Mastersheet')
+
+# write the DataFrame to an SQLite staging table
+df.to_sql('stg_excelsheet', conn, if_exists='replace', index=False)
+
+# create staging view
+with open('venv/DDL_scripts/Staging/stg_excelsheet_vw.sql', 'r') as file:
+    sql_script = file.read()
+conn.executescript(sql_script)
+# ------------------------------------------------------------------
+
+
+# ----------- CREATE RAW DATA VAULT OBJECTS ------------------------
+# rdv_film_hub.sql
+with open('venv/DDL_scripts/Raw_data_vault/rdv_film_hub.sql', 'r') as file:
+    sql_script = file.read()
+conn.executescript(sql_script)
+
+# rdv_film_sat.sql
+with open('venv/DDL_scripts/Raw_data_vault/rdv_film_sat.sql', 'r') as file:
+    sql_script = file.read()
+conn.executescript(sql_script)
+
+# rdv_meister_hub.sql
+with open('venv/DDL_scripts/Raw_data_vault/rdv_meister_hub.sql', 'r') as file:
+    sql_script = file.read()
+conn.executescript(sql_script)
+
+# rdv_filmavond_link.sql
+with open('venv/DDL_scripts/Raw_data_vault/rdv_filmavond_link.sql', 'r') as file:
+    sql_script = file.read()
+conn.executescript(sql_script)
+
+# rdv_filmavond_sat.sql
+with open('venv/DDL_scripts/Raw_data_vault/rdv_filmavond_sat.sql', 'r') as file:
+    sql_script = file.read()
+conn.executescript(sql_script)
+
+# ------------------------------------------------------------------
+
+
+# ----------- RUN DML SCRIPTS: From_STG_to_RDV----------------------
+#  Load hubs
+with open('venv/DML_scripts/From_STG_to_RDV/load_hubs.sql', 'r') as file:
+    sql_script = file.read()
+conn.executescript(sql_script)
+conn.commit()
+
+# Load links
+with open('venv/DML_scripts/From_STG_to_RDV/load_links.sql', 'r') as file:
+    sql_script = file.read()
+conn.executescript(sql_script)
+conn.commit()
+
+# Load satalites
+with open('venv/DML_scripts/From_STG_to_RDV/load_sats.sql', 'r') as file:
+    sql_script = file.read()
+conn.executescript(sql_script)
+conn.commit()
+
+# ------------------------------------------------------------------
+
+# close the database connection
+conn.close()
