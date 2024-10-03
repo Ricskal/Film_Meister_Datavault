351e93f70eae45757ba55a731b7620458be7fd64
Ricskal <rickenisnkrullen@gmail.com>
2024-10-03 11:30:18 +0200

    save

--- etl/ddl_scripts_postgres/business_data_vault/views/bdv_film_sat.sql
+++ etl/ddl_scripts_postgres/business_data_vault/views/bdv_film_sat.sql
@@ -9,9 +9,9 @@ CREATE OR REPLACE VIEW bdv.film_sat as
         SELECT
               rfs.Film_Hub_Key
             , rfs.Film_Title
-            , CAST(rfs.Film_Jaar AS INTEGER) AS Film_Jaar
+            , rfs.Film_Jaar AS Film_Jaar
             , rfs.Film_Tijdsduur_min
-            , CAST(COALESCE(rfs.Film_IMDB_Score, '-1') AS REAL) AS Film_IMDB_Score
+            , COALESCE(rfs.Film_IMDB_Score, -1) AS Film_IMDB_Score
             , CAST(1 AS REAL) / gc.Genre_Count AS Genre_Weging
         FROM rdv.film_sat rfs
         JOIN GenreCounts gc
