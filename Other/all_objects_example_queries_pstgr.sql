89c0fba80d2c620e77bc0eaab4dfe07fd0498c6f
Ricskal <rickenisnkrullen@gmail.com>
2024-10-03 12:17:56 +0200

    save

---
+++ Other/all_objects_example_queries_pstgr.sql
@@ -0,0 +1,75 @@
+-- Drop tables --
+drop table if exists bdv.film_genre_link;
+drop table if exists bdv.genre_hub;
+drop table if exists dm.dim_datum;
+drop table if exists rdv.film_hub;
+drop table if exists rdv.film_sat;
+drop table if exists rdv.filmavond_link;
+drop table if exists rdv.filmavond_sat;
+drop table if exists rdv.meister_hub;
+drop table if exists stg.excelsheet;
+
+-- Drop views --
+drop view if exists bdv.film_hub;
+drop view if exists bdv.film_sat;
+drop view if exists bdv.filmavond_link;
+drop view if exists bdv.filmavond_sat;
+drop view if exists bdv.meister_hub;
+drop view if exists dm.dim_datum_vw;
+drop view if exists dm.dim_film;
+drop view if exists dm.dim_meister;
+drop view if exists dm.fact_filmavond;
+drop view if exists stg.excelsheet_vw;
+drop view if exists dm.dim_film_genre;
+
+-- STG --
+select * from stg.excelsheet;
+select * from stg.excelsheet_vw;
+
+-- RDV --
+select * from rdv.film_hub;
+select * from rdv.film_sat;
+select * from rdv.filmavond_link;
+select * from rdv.filmavond_sat;
+select * from rdv.meister_hub;
+
+select *
+from rdv.filmavond_link rfl
+join rdv.filmavond_sat rfs on rfl.Filmavond_Link_Key = rfs.Filmavond_Link_Key
+join rdv.film_hub rfh on rfl.Film_Hub_Key = rfh.Film_Hub_Key
+join rdv.film_sat rfs2 on rfh.Film_Hub_Key =  rfs2.Film_Hub_Key
+join rdv.meister_hub rmh on rfl.Meister_Hub_Key = rmh.Meister_Hub_Key;
+
+-- BDV --
+select * from bdv.film_hub;
+select * from bdv.film_sat;
+select * from bdv.filmavond_link;
+select * from bdv.filmavond_sat;
+select * from bdv.meister_hub;
+select * from bdv.film_genre_link;
+select * from bdv.genre_hub;
+
+select *
+from bdv.filmavond_link bfl
+join bdv.filmavond_sat bfs on bfl.Filmavond_Link_Key = bfs.Filmavond_Link_Key
+join bdv.film_hub bfh on bfl.Film_Hub_Key = bfh.Film_Hub_Key
+join bdv.film_sat bfs2 on bfh.Film_Hub_Key = bfs2.Film_Hub_Key
+join bdv.meister_hub bmh on bfl.Meister_Hub_Key = bmh.Meister_Hub_Key
+join bdv.film_genre_link bfgl on bfh.Film_Hub_Key = bfgl.Film_Hub_Key
+join bdv.genre_hub bgh on bfgl.Genre_Hub_Key = bgh.Genre_Hub_Key;
+
+-- DM --
+select * from dm.fact_filmavond dff;
+select * from dm.dim_film ddf;
+select * from dm.dim_meister ddm;
+select * from dm.dim_datum ddd;
+select * from dm.dim_datum_vw ddv;
+select * from dm.dim_film_genre;
+
+select *
+from dm.fact_filmavond dff
+join dm.dim_film ddf on dff.Dim_Film_key = ddf.Dim_Film_Key
+join dm.dim_meister ddm on dff.Dim_Meister_key = ddm.Dim_Meister_Key
+join dm.dim_film_genre dfg on dff.Dim_Film_key = dfg.Dim_Film_Key
+--join dm.dim_datum ddd on dff.Dim_Filmavond_Datum_key = ddd.Dim_Datum_Key
+join dm.dim_datum_vw ddv on dff.Dim_Filmavond_Datum_key = ddv.Dim_Datum_Key;
--- etl/ddl_scripts_postgres/datamarts/views/dm_fact_filmavond.sql
+++ etl/ddl_scripts_postgres/datamarts/views/dm_fact_filmavond.sql
@@ -4,7 +4,7 @@ CREATE OR REPLACE VIEW dm.fact_filmavond AS
         , bfl.Film_Hub_Key AS Dim_Film_key
         , bfl.Meister_Hub_Key AS Dim_Meister_key
         , bfl.Datum_Filmavond_Key AS Dim_Filmavond_Datum_key
-        , CAST('1' AS INTEGER) AS Aantal_Filmavonden
+        , 1 AS Aantal_Filmavonden
         , bfs.Ind_Gezien AS Film_Al_Gezien
         , bfs.Aantal_Jaar AS Aantal_Jaar_Filmavond
         , bfs.Aantal_Ronde AS Aantal_Ronde_Filmavond
