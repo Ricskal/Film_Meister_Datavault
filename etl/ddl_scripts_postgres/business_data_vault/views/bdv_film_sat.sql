c1224ce317b558832b4699aa32f460a913670bca
Ricskal <rickenisnkrullen@gmail.com>
2024-10-24 22:34:30 +0200

    bugfix

--- etl/ddl_scripts_postgres/business_data_vault/views/bdv_film_sat.sql
+++ etl/ddl_scripts_postgres/business_data_vault/views/bdv_film_sat.sql
@@ -14,7 +14,7 @@ CREATE OR REPLACE VIEW bdv.film_sat as
             , COALESCE(rfs.Film_IMDB_Score, -1) AS Film_IMDB_Score
             , CAST(1 AS REAL) / gc.Genre_Count AS Genre_Weging
         FROM rdv.film_sat rfs
-        JOIN GenreCounts gc
+        LEFT JOIN GenreCounts gc
             ON rfs.Film_Hub_Key = gc.Film_Hub_Key
         WHERE rfs.Is_Current = 1
 ;
